189100231fddb79ba7e208d982ea5507386ef7a2
crod2 <carlarodriguez90@gmail.com>
2019-05-02 23:47:51 +0200

    Get data from API on main.js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('>> Ready :)');
+const title = document.querySelector('.page__title');
+const input = document.querySelector('.input');
+const button = document.querySelector('.btn');
+const list = document.querySelector('.page__cards');
+
+const API = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
+const cardsNumber = input.value;
+
+const showCards = () => {
+  fetch(`${API}${cardsNumber}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+}
+
+button.addEventListener('click', showCards);
