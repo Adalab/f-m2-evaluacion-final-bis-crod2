32f715afc265b3c239b398e18ef15296b7f2b1e4
crod2 <carlarodriguez90@gmail.com>
2019-05-05 19:04:01 +0200

    Fix how to get data dependind on input values

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,19 +1,36 @@
 'use strict';
 
 const title = document.querySelector('.page__title');
-const input = document.querySelector('.input');
+const inputs = document.querySelectorAll('.input');
 const button = document.querySelector('.btn');
 const list = document.querySelector('.page__cards');
 
 const API = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
-const cardsNumber = input.value;
 
 const showCards = () => {
-  fetch(`${API}${cardsNumber}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
+  for (const input of inputs) {
+    const inputValue = input.value;
+    const inputChecked = input.checked;
+    if (inputChecked === true) {
+      fetch(`${API}${inputValue}`)
+        .then(response => response.json())
+        .then(data => {
+        console.log(data);
+      })
+    }
+  }
 }
+//
+//     for (const item of data) {
+//       const newItem = document.createElement('li');
+//       const cardFront = document.createElement('img');
+//       cardFront.src = item.image;
+
+//       list.appendChild(newItem);
+//       newItem.appendChild(cardFront);
+//     }
+//   })
+ //}
+
 
 button.addEventListener('click', showCards);
